f226c6fedf074fecdda337afac0a3cc4765b478c
Santhoshmurali13 <153189600+Santhoshmurali13@users.noreply.github.com>
2025-09-04 21:32:57 +0530

    basepage resolved

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,7 @@
 from actions.webpage_actions import WebAppActions
 from conftest import readConstants
-from appium import webdriver
+from selenium import webdriver
+from appium import webdriver as appiumdriver
 from actions.android_actions import AndroidActions
 from actions.ios_actions import iOSActions
 
@@ -9,10 +10,12 @@ from actions.ios_actions import iOSActions
 class BasePage:
 
     def __init__(self, driver):
-        self.driver = driver
+        self.driver = driver 
         if isinstance(driver, webdriver.Remote):
-            print("Application Launched")
             self.driver = driver
+            print("Web browser instance")
+            self.actions = WebAppActions(driver)
+        elif isinstance(driver, appiumdriver.Remote):
             capabilities = driver.capabilities
             platform_name = capabilities.get('platformName', '').lower()
             if platform_name == 'android':
@@ -21,25 +24,19 @@ class BasePage:
             elif platform_name == 'ios':
                 print("Inside iOS")
                 self.actions = iOSActions(driver)
-            else:
-                print("for WebBrowser")
-                self.actions = WebAppActions(driver)
-
-
-    
-    def launch_application(self, appURL):
-        if appURL:
-            self.actions.launch_browser_url(appURL)
-            print("Opened McD Website")
         else:
-            self.actions.launch_app()
-            print("Opened McD App")   
+            print("Both chrome and webdriver instance is not called")
 
-    
+
+    def launch_application(self):
+        self.actions.launch_app()
+        print("Opened McD App")
+
+
     def open_mcd_website(self):
         url = readConstants("APP_URL")
         self.actions.launch_browser_url(url)
-        self.driver.maximize_window() 
+        self.driver.maximize_window()
         print("Opened McD Website")
 
 
@@ -47,5 +44,11 @@ class BasePage:
         self.actions.quit_Driver()
         print("Closed McD App")
 
-
-
+    # def launch_application(self, appURL):
+    #     if appURL:
+    #         self.actions.launch_browser_url(appURL)
+    #         print("Opened McD Website")
+    #     else:
+    #         self.actions.launch_app()
+    #         print("Opened McD App")  
+            
